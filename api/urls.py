1ff602e1c45af400ff48167cf0cc8ea840bb3b9f
Eric <58400036+echeung52@users.noreply.github.com>
2024-02-17 15:08:44 -0500

    token api

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import getRecommendations, search, getTrack
+from .views import getRecommendations, search, getTrack, getToken
 
 
 urlpatterns = [
     path('recommendations/', getRecommendations, name='recommendations'),
     path('search/<str:song>', search, name='search'),
     path('track/', getTrack, name='track'),
+    path('token/', getToken, name='token'),
 ]
--- api/views.py
+++ api/views.py
@@ -4,10 +4,13 @@ import os
 import base64
 from requests import post, get
 import json
+from django.core.cache import cache
 
-client_id = os.environ.get("CLIENT_ID")
-client_secret = os.environ.get("CLIENT_SECRET")
+# client_id = os.environ.get("CLIENT_ID")
+# client_secret = os.environ.get("CLIENT_SECRET")
 
+client_id = 'a058da0fea2a4e6d888f222e9e288387'
+client_secret = '5ed52bc698b74801aa4f7847fa41c27b'
 
 def get_token():
     auth_string = client_id + ':' + client_secret
@@ -24,6 +27,23 @@ def get_token():
     json_result = json.loads(result.content)
     token = json_result['access_token']
     return token
+
+@api_view(['GET'])
+def getToken(request):
+    auth_string = client_id + ':' + client_secret
+    auth_bytes = auth_string.encode('utf-8')
+    auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
+
+    url = 'https://accounts.spotify.com/api/token'
+    headers = {
+        'Authorization': 'Basic ' + auth_base64,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {'grant_type': 'client_credentials'}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result['access_token']
+    return Response({"token":token})
 
 def get_auth_header(token):
     return {"Authorization": "Bearer " + token}
